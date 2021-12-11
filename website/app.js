174759853c4c92f62d3b60ea19da0250a1de93aa
Marwan Mohamed Taha Abdelazim Abdelsamad <90199950+Marwan-Taha@users.noreply.github.com>
2021-12-12 01:37:30 +0200

    Add files via upload

---
+++ website/app.js
@@ -0,0 +1,55 @@
+//Global variables
+const apiKey = '&appid=04f9797ec49488175265f879ff318b8b&units=metric';
+const URL = `https://api.openweathermap.org/data/2.5/weather?zip=`;
+let d = new Date();
+let newDate = d.getMonth()+1 +'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click' , ()=>{
+    
+    const zipCode = document.getElementById('zip').value;
+    getWeatherData(URL , zipCode , apiKey)
+})
+
+
+
+const getWeatherData = async (url,zip,key)=>{
+
+    //GET Web API Data
+    //GET Request
+    const response = await fetch(url+zip+key);
+    
+        const data = await response.json();
+        const temperature = data.main.temp;
+        console.log(temperature);
+    
+    //User entered feelings    
+    const feeling = document.getElementById('feelings').value;
+
+    //POST Request
+    await fetch('/all' , {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers : {
+            'Content-Type' : 'application/json',            
+        },
+        body: JSON.stringify({
+            temperature ,
+            newDate,
+            feeling,
+        }),
+    });
+
+
+    //Updating UI
+    const res = await fetch('/get') ;
+    const data2 = await res.json();
+    document.getElementById('date').innerHTML = `Date: ${data2.newDate}`;
+    document.getElementById('Temperature').innerHTML = `Temperature is ${data2.temperature}&#8451;`;
+    document.getElementById('enteredFeelings').innerHTML = `I feel: ${data2.feeling}`;
+}
+
+
+
+
+
